6a7e70773eda8cf838aed36ebb6d748904039c5e
David Nguyen <ndtuan89@gmail.com>
2018-09-21 17:49:47 +0700

    update new version WannaCry

--- src/js/airdrop-game.js
+++ src/js/airdrop-game.js
@@ -1,102 +1,111 @@
-import {
-  abi
-} from "./contract/minigames/airdrop-game"
-
+import { abi } from "./contract/minigames/airdrop-game";
+const Config = require("../../../config/config");
 
 export const AirdropGame = {
-  contract_address: '0x5b813a2f4b58183d270975ab60700740af00a3c9',
-  //contract_address: '0xe06a2ae710004780026aab950f2d4290bc7b43bc',
+  contract_address: Config.airdropGameContractAddress,
   miniGameContract: null,
   miniGameContract_with_provider: null,
   startAirdropGame(callback) {
     if (typeof web3 != 'undefined') {
-      AirdropGame.contract_init(callback)
+      this.contract_init(callback)
     } else {
-      AirdropGame(false)
+      this(false)
     }
   },
   contract_init(callback) {
     if (typeof web3.eth.accounts[0] != 'undefined') {
-      AirdropGame.miniGameContract = web3.eth
+      this.miniGameContract = web3.eth
         .contract(abi)
-        .at(AirdropGame.contract_address)
-      AirdropGame.miniGameContract_with_provider = game.web3.eth
+        .at(this.contract_address)
+      this.miniGameContract_with_provider = game.web3.eth
         .contract(abi)
-        .at(AirdropGame.contract_address)
+        .at(this.contract_address)
       callback(true)
     } else {
       callback(false)
     }
   },
   claimCrystal ( callback ) {
-    AirdropGame.miniGameContract.claimCrystal.sendTransaction(
-      {
-        from: web3.eth.accounts[0],
-        gas: web3.toHex(400000),
-        gasPrice: game.default_gas_price
-      },
-      function (err, result) {
-        if (!err) {
-          callback()
+    this.miniGameContract
+      .claimCrystal
+      .sendTransaction(
+        {
+          from: web3.eth.accounts[0],
+          gas: web3.toHex(400000),
+          gasPrice: game.default_gas_price
+        },
+        function (err, result) {
+          if (!err) {
+            callback()
+          }
         }
-      }
-    );
+      );
   },
   getCurrentMiniGameId( callback ) {
-    AirdropGame.miniGameContract_with_provider.miniGameId.call({
-        from: web3.eth.accounts[0]
-    },
-    function (err, result) {
-        return callback( err, result );
-      }
-    );
+    this.miniGameContract_with_provider
+      .miniGameId
+      .call({
+          from: web3.eth.accounts[0]
+      },
+      function (err, result) {
+          return callback( err, result );
+        }
+      );
   },
   getMiniGame ({ miniGameId }, callback ) {
-     AirdropGame.miniGameContract_with_provider.minigames.call(
-      miniGameId,
-      {
-          from: web3.eth.accounts[0]
-      },
-      function (err, result) {
-          return callback(err, result);
-        }
-    );
+     this.miniGameContract_with_provider
+       .minigames
+       .call(
+        miniGameId,
+        {
+            from: web3.eth.accounts[0]
+        },
+        function (err, result) {
+            return callback(err, result);
+          }
+      );
   },
   getPlayerAirdropGameData ( callback ) {
-    AirdropGame.miniGameContract_with_provider.players.call(
-      web3.eth.accounts[0],
-      {
-          from: web3.eth.accounts[0]
-      },
-      function (err, result) {
-          return callback(err, result);
-        }
-    );
+    this.miniGameContract_with_provider
+      .players
+      .call(
+        web3.eth.accounts[0],
+        {
+            from: web3.eth.accounts[0]
+        },
+        function (err, result) {
+            return callback(err, result);
+          }
+      );
   },
   getShareCrystalByMiniGameId ( { miniGameId }, callback ) {
-    AirdropGame.miniGameContract_with_provider.calculateShareCrystal.call(
-      miniGameId,
-      {
-          from: web3.eth.accounts[0]
-      },
-      function (err, result) {
-          return callback(err, result);
-        }
-    );
+    this.miniGameContract_with_provider
+      .calculateShareCrystal
+      .call(
+        miniGameId,
+        {
+            from: web3.eth.accounts[0]
+        },
+        function (err, result) {
+            return callback(err, result);
+          }
+      );
   },
   joinAirdropGame ( callback ) {
-    AirdropGame.miniGameContract.joinMiniGame.sendTransaction(
-      {
-        from: web3.eth.accounts[0],
-        gas: web3.toHex(400000),
-        gasPrice: game.default_gas_price
-      },
-      function (err, result) {
-        if (!err) {
-          callback()
+    this.miniGameContract
+      .joinMiniGame
+      .sendTransaction(
+        {
+          from: web3.eth.accounts[0],
+          gas: web3.toHex(400000),
+          gasPrice: game.default_gas_price
+        },
+        function (err, result) {
+          if (!err) {
+            callback()
+          }
         }
-      }
-    );
+      );
   },
   toETH: number => {
     return web3.fromWei(number, 'ether')
--- src/js/game.js
+++ src/js/game.js
@@ -4,14 +4,61 @@ game.web3 = {};
 
 game.default_gas_price = 0;
 
+/** ----------------------------------------------------------------------
+* MINING WAR GAME INFORMATION
+* ------------------------------------------------------------------------
+*/
+game.user = {
+    "address": '',
+    "player_balance": 0,
+    "crystals": 0,
+    "lastupdate": 0,
+    "hashratePerDay": 0,
+    "miners": [],
+    "hasBoost": 0,
+    "referral_count": 0,
+    "yourQuest": 0,
+    "unconfirmedCrystals": 0,
+};
+// miner data
+game.minerData = [];
+game.minerData[0] = { "price": 10,      "hashrate": 10,    "limit": 10 };
+game.minerData[1] = { "price": 100,     "hashrate": 200,   "limit": 2 };
+game.minerData[2] = { "price": 400,     "hashrate": 800,   "limit": 4 };
+game.minerData[3] = { "price": 1600,    "hashrate": 3200,  "limit": 8 };
+game.minerData[4] = { "price": 6400,    "hashrate": 9600,  "limit": 16 };
+game.minerData[5] = { "price": 25600,   "hashrate": 38400, "limit": 32 };
+game.minerData[6] = { "price": 204800,  "hashrate": 204800,"limit": 64 };
+game.minerData[7] = { "price": 1638400, "hashrate": 819200,"limit": 65536 };
+// booster
+game.boostData = [];
+game.boostData[0] = { "owner": '', "boostRate": 150, "price": 0.01 };
+game.boostData[1] = { "owner": '', "boostRate": 175, "price": 0.01 };
+game.boostData[2] = { "owner": '', "boostRate": 200, "price": 0.01 };
+game.boostData[3] = { "owner": '', "boostRate": 225, "price": 0.01 };
+game.boostData[4] = { "owner": '', "boostRate": 250, "price": 0.01 };
+
+game.miningWarRoundNumber = 0;
+
+game.referral = 0;
+
 game.CRTSTAL_MINING_PERIOD = 86400;
+game.VIRUS_MINING_PERIOD = 86400;
 game.numberOfMiners = 8;
 game.numberOfBoosts = 5;
 game.numberOfOrders = 5;
 
 game.deadline = Date.now;
 game.showLottery = false;
-game.currentMiniGameId = 0; // is mini game of airdrop game
+
+game.sponsor_fee = 0.01;
+
+game.rank_list = [];
+
+/** ----------------------------------------------------------------------
+* AIRDROP GAME INFORMATION
+* ------------------------------------------------------------------------
+*/
 game.currentAirdropGameData = {
     miningWarRoundNumber: 0,
     ended: 0,
@@ -24,115 +71,101 @@ game.currentAirdropGameData = {
 game.lastAirdropGameData = {
     prizeCrystal: 0,
     totalJoin: 0,
-    playerWin: ''    
+    playerWin: '0x0000000000000000000000000000000000000000'    
 };
-game.minigames = []
+game.minigames = [];
+game.currentMiniGameId = 0; // is mini game of airdrop game
 game.playerAirdropGame = { 
-    win: 0,
-    share: 0,
-    crystal: 0,
-    totalJoin: 0,
-    currentMiniGameId: 0
+    "win": 0,
+    "share": 0,
+    "crystal": 0,
+    "totalJoin": 0,
+    "currentMiniGameId": 0
+};
+/** ----------------------------------------------------------------------
+* ENGINEER INFORMATION
+* ------------------------------------------------------------------------
+*/
+// list players
+game.players = [];
+// game sponsor info
+game.gameSponsor = "";
+game.gameSponsorPrice = 0;
+
+game.engineerPlayer = {
+    "engineerRoundNumber": 0,
+    "virusNumber": 0,
+    "virusDefence": 0,
+    "research": 0,
+    "researchPerDay": 0,
+    "lastUpdateTime": 0,
+    "engineers": [0,0,0,0,0,0,0,0],
+    "hasBooster": false,
+    "nextTimeAtk": 0,
+    "endTimeUnequalledDef": 0
+}
+// engineer
+game.engineerData = [];
+game.engineerData[0] = { "basePrice": 10,      "baseEth": 0,    "baseResearch": 10,    "limit": 10 };
+game.engineerData[1] = { "basePrice": 50,      "baseEth": 0.01,"baseResearch": 200,   "limit": 2 };
+game.engineerData[2] = { "basePrice": 200,     "baseEth": 0.02, "baseResearch": 800,   "limit": 4 };
+game.engineerData[3] = { "basePrice": 800,     "baseEth": 0.04, "baseResearch": 3200,  "limit": 8 };
+game.engineerData[4] = { "basePrice": 3200,    "baseEth": 0.08, "baseResearch": 9600,  "limit": 16 };
+game.engineerData[5] = { "basePrice": 12800,   "baseEth": 0.16, "baseResearch": 38400, "limit": 32 };
+game.engineerData[6] = { "basePrice": 102400,  "baseEth": 0.32, "baseResearch": 204800,"limit": 64 };
+game.engineerData[7] = { "basePrice": 819200,  "baseEth": 0.64, "baseResearch": 819200,"limit": 65536 };
+//booster
+game.EngineerboostData = [];
+game.EngineerboostData[0] = { "owner": '', "boostRate": 150, "price": 0.01 };
+game.EngineerboostData[1] = { "owner": '', "boostRate": 175, "price": 0.02 };
+game.EngineerboostData[2] = { "owner": '', "boostRate": 200, "price": 0.04 };
+game.EngineerboostData[3] = { "owner": '', "boostRate": 225, "price": 0.08 };
+game.EngineerboostData[4] = { "owner": '', "boostRate": 250, "price": 0.16 };
+             //level   1         2        3          4       5      6         7           8
+game.engineersName = ["Link", "Mouse", "Switch", "Dozer", "Tank", "Apoc", "Trinity", "Morpheus"];
+
+game.currentVirus = 0;
+
+game.historyWars = [];
+
+game.engineerRoundNumber = 0;
+/** ----------------------------------------------------------------------
+* BOSS WANNACRY INFORMATION
+* ------------------------------------------------------------------------
+*/
+game.bossWannaCryPlayer = {
+    "currentBossRoundNumber": 0,
+    "lastBossRoundNumber": 0,
+    "win": 0,
+    "share": 0,
+    "dame": 0,
+    "nextTimeAtk": 0
+}
+game.currentBossWannaCry = {
+    "bossRoundNumber": 0,
+    "bossHp": 0,
+    "def": 0,
+    "prizePool": 0,
+    "playerLastAtk": 0,
+    "totalDame": 0,
+    "ended": false
+}
+game.lastBossWannaCry = {
+    "bossRoundNumber": 0,
+    "bossHp": 0,
+    "def": 0,
+    "prizePool": 0,
+    "playerLastAtk": 0,
+    "totalDame": 0,
+    "ended": false   
 }
 
-game.referral = 0;
+game.dameCurrentBossWannaCry = 0;
 
-game.user = {
-    address: '',
-	player_balance: 0,
-    crystals: 0,
-    lastupdate: 0,
-    hashratePerDay: 0,
-    miners: [],
-    hasBoost: 0,
-    referral_count: 0,
-    yourQuest: 0,
-    unconfirmedCrystals: 0,
-};
+game.yourRewardBossWannaCry = 0;
 
-game.sponsor_fee = 0.01;
 
-game.rank_list = [];
-
-// BOOSTER
-game.boostData = [];
-
-game.boostData[0] = {
-    owner: '',
-    boostRate: 150,
-    price: 0.01
-};
-game.boostData[1] = {
-    owner: '',
-    boostRate: 175,
-    price: 0.01
-};
-game.boostData[2] = {
-    owner: '',
-    boostRate: 200,
-    price: 0.01
-};
-game.boostData[3] = {
-    owner: '',
-    boostRate: 225,
-    price: 0.01
-};
-game.boostData[4] = {
-    owner: '',
-    boostRate: 250,
-    price: 0.01
-};
-
-// minerData[0] = MinerData(10, 10, 10); //lv1
-// minerData[1] = MinerData(100, 200, 10); //lv2
-// minerData[2] = MinerData(400, 800, 10); //lv3
-// minerData[3] = MinerData(1600, 3200, 10); //lv4
-// minerData[4] = MinerData(6400, 12800, 10); //lv5
-// minerData[5] = MinerData(25600, 51200, 10); //lv6
-// minerData[6] = MinerData(204800, 409600, 10); //lv7
-// minerData[7] = MinerData(1638400, 1638400, 999); //lv8
-game.minerData = [];
-
-game.minerData[0] = {
-    price: 10,
-    hashrate: 10,
-    limit: 10
-};
-game.minerData[1] = {
-    price: 100,
-    hashrate: 200,
-    limit: 2
-};
-game.minerData[2] = {
-    price: 400,
-    hashrate: 800,
-    limit: 4
-};
-game.minerData[3] = {
-    price: 1600,
-    hashrate: 3200,
-    limit: 8
-};
-game.minerData[4] = {
-    price: 6400,
-    hashrate: 9600,
-    limit: 16
-};
-game.minerData[5] = {
-    price: 25600,
-    hashrate: 38400,
-    limit: 32
-};
-game.minerData[6] = {
-    price: 204800,
-    hashrate: 204800,
-    limit: 64
-};
-game.minerData[7] = {
-    price: 1638400,
-    hashrate: 819200,
-    limit: 65536
-};
+// buy and sell info not use in current version
 
 game.sellOrderData = [];
 
